ae2e41a9aa06ad94c919a8548ff06c36cffbf1be
Vadym Stupakov <v.stupakov@samsung.com>
2017-05-16 14:12:52 +0300

    Implemented read function

--- chardev.c
+++ chardev.c
@@ -2,10 +2,14 @@
 #include <linux/module.h>
 #include <linux/file.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 
 #include "chardev_log.h"
 
 static int dev0_major = 0;
+static __u8* buf = NULL;
+static __u32 buf_size = 20;
 
 int chardev_init(const char* device_name, const struct file_operations* f_ops) {
     const int device_major = register_chrdev(0, device_name, f_ops);
@@ -25,11 +29,34 @@ void chardev_cleanup(int device_major, const char* device_name)
 
 int chardev_open(struct inode* inode, struct file* file){
     try_module_get(THIS_MODULE);
+    buf = kmalloc(buf_size, GFP_KERNEL);
+    memset(buf, '@', buf_size);
     return 0;
 }
 
-ssize_t chardev_read(struct file* file, char __user * buf, size_t size, loff_t* offset){
-    return size;
+ssize_t chardev_read(struct file* file, char __user * usr_buf, size_t size, loff_t* offset){
+    int ret = -1;
+    int count = 3;
+    if(*offset >= buf_size){
+        return 0;
+    }
+
+    if(*offset + count > buf_size){
+        count = buf_size - offset;
+    }
+
+    if((ret = copy_to_user(usr_buf, buf + *offset, count))){
+        CHARDEV_LOG_CRIT("%s", "copy_to_user() error!\n");
+        CHARDEV_LOG_CRIT("ret = %d\n", ret);
+        return 0;
+    }
+
+    *offset += count;
+
+    CHARDEV_LOG_INFO("size   = %ld\n", size);
+    CHARDEV_LOG_INFO("offset = %lld\n", *offset);
+
+    return count;
 }
 
 ssize_t chardev_write(struct file* file, const char __user * buf, size_t size, loff_t* offset){
@@ -38,6 +65,7 @@ ssize_t chardev_write(struct file* file, const char __user * buf, size_t size, l
 
 int chardev_release(struct inode* inode, struct file* file){
     module_put(THIS_MODULE);
+    kfree(buf);
     return 0;
 }
 
